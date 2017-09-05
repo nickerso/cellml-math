0c3155902f29e0f437bf3c9c84ead0149029e2d0
David Nickerson <nickerso@users.sourceforge.net>
2017-09-05 22:55:40 +1200

    Missed a check on parsing success leading to segfault.

--- src/modelloader.cpp
+++ src/modelloader.cpp
@@ -30,36 +30,37 @@ bool ModelLoader::loadModel(const std::string& modelUrl,
 {
     std::string url = XmlDoc::buildAbsoluteUri(modelUrl, baseUrl);
     XmlDoc modelDoc;
-    if (modelDoc.parseDocument(url) == 0)
+    if (modelDoc.parseDocument(url) != 0)
     {
-        std::string modelString = modelDoc.dumpString();
-        // Parse
-        libcellml::Parser parser(libcellml::Format::XML);
-        libcellml::ModelPtr model = parser.parseModel(modelString);
-        if (0 != parser.errorCount())
-        {
-            std::cerr << "Error parsing the CellML model: " << url
-                      << std::endl;
-            for (size_t i = 0; i < parser.errorCount(); ++i) {
-                std::cerr << parser.getError(i)->getDescription() << std::endl;
-            }
-            return false;
-        }
-        libcellml::Validator validator;
-        validator.validateModel(model);
-        if (0 != validator.errorCount())
-        {
-            std::cerr << "Error validating the CellML model: " << url
-                      << std::endl;
-            for (size_t i = 0; i < validator.errorCount(); ++i) {
-                std::cerr << validator.getError(i)->getDescription()
-                          << std::endl;
-            }
-            //return false;
-        }
-        pImpl->model = model;
-        pImpl->url = url;
+        return false;
     }
+    std::string modelString = modelDoc.dumpString();
+    // Parse
+    libcellml::Parser parser(libcellml::Format::XML);
+    libcellml::ModelPtr model = parser.parseModel(modelString);
+    if (0 != parser.errorCount())
+    {
+        std::cerr << "Error parsing the CellML model: " << url
+                  << std::endl;
+        for (size_t i = 0; i < parser.errorCount(); ++i) {
+            std::cerr << parser.getError(i)->getDescription() << std::endl;
+        }
+        return false;
+    }
+    libcellml::Validator validator;
+    validator.validateModel(model);
+    if (0 != validator.errorCount())
+    {
+        std::cerr << "Error validating the CellML model: " << url
+                  << std::endl;
+        for (size_t i = 0; i < validator.errorCount(); ++i) {
+            std::cerr << validator.getError(i)->getDescription()
+                      << std::endl;
+        }
+        //return false;
+    }
+    pImpl->model = model;
+    pImpl->url = url;
     return true;
 }
 
